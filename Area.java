0ec25dd0ee587596ad23cca9d03ddc26175bd5e5
GUSTAVO HENRIQUE DE OLIVEIRA <04383868096@senacrs.edu.br>
2024-11-28 20:20:21 -0300

    Implement_area
    Classe Area mais implementada.

--- Area.java
+++ Area.java
@@ -13,15 +13,40 @@ public class Area {
     String descricao;
     Boolean atalho;
     Boolean bau;
-    Inimigo inimigos;
+    String[] inimigos;
     
-    public Area(String nome_area, String descricao_area, Boolean atalho_presente,  Boolean bau_na_area){
+    public Area(String nome_area, String descricao_area, Boolean atalho_presente,  Boolean bau_na_area, String[] inimigos_area){
         nome = nome_area;
         descricao = descricao_area;
         atalho = atalho_presente;
         bau = bau_na_area;
-        inimigos = [];
+        inimigos = inimigos_area;
+    }
+   
+    
+    public void realizar_atalho(Area area){
+       if(atalho == true){
+           
+       }
     }
     
+    public String getNome(){
+        return nome;
+    }
     
+    public String getDescricao(){
+        return descricao;
+    }
+    
+    public Boolean getAtalho(){
+        return atalho;
+    }
+    
+    public Boolean getBau(){
+        return bau;
+    }
+    
+    public String[] getInimigos(){
+        return inimigos;
+    }
 }
